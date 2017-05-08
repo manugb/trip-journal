000ab6c4c062f09b0cca3c26d37ad30c8094f59c
Manuel Gambino <manuelgambinob@gmail.com>
2017-05-07 22:27:35 -0300

    Add recycler view

--- app/src/main/java/utn_frba_mobile/dadm_diario_viajes/NotesActivity.java
+++ app/src/main/java/utn_frba_mobile/dadm_diario_viajes/NotesActivity.java
@@ -2,12 +2,31 @@ package utn_frba_mobile.dadm_diario_viajes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class NotesActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.notes_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
