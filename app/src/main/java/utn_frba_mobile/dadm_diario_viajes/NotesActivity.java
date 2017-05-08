576da3631b3c004c5f8a5a06309ba01cb284f0a7
Manuel Gambino <manuelgambinob@gmail.com>
2017-05-07 22:17:28 -0300

    Add empty activity

---
+++ app/src/main/java/utn_frba_mobile/dadm_diario_viajes/NotesActivity.java
@@ -0,0 +1,13 @@
+package utn_frba_mobile.dadm_diario_viajes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes);
+    }
+}
