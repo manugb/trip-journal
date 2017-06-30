524f01561d6a627848b6ddb88e966c3407659e81
wuacamolegs <wuacamolegs@gmail.com>
2017-06-30 20:48:10 -0300

    splash screen con logo y nombre de la app

---
+++ app/src/main/java/utn_frba_mobile/dadm_diario_viajes/activities/SplashActivity.java
@@ -0,0 +1,51 @@
+package utn_frba_mobile.dadm_diario_viajes.activities;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import utn_frba_mobile.dadm_diario_viajes.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    // Set the duration of the splash screen
+    private static final long SPLASH_SCREEN_DELAY = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set portrait orientation
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        // Hide title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.splash_screen);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+
+                // Start the next activity
+                Intent authIntent = new Intent().setClass(
+                        SplashActivity.this, AuthUiActivity.class);
+                startActivity(authIntent);
+
+                // Close the activity so the user won't able to go back this
+                // activity pressing Back button
+                finish();
+            }
+        };
+
+        // Simulate a long loading process on application startup.
+        Timer timer = new Timer();
+        timer.schedule(task, SPLASH_SCREEN_DELAY);
+    }
+
+}
+
